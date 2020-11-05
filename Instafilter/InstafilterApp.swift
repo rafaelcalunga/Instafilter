ff7806fbca268783b231e3dd5a3297f250369fa3
Rafael Calunga <rafaelcalunga@gmail.com>
2020-11-05 18:09:47 -0500

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Rafael Calunga on 2020-11-05.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
