86f730d3ec8a51b49d0a033bc3a5b586189b5e27
Rafael Calunga <rafaelcalunga@gmail.com>
2020-11-08 12:39:27 -0500

    Integrating Core Image with SwiftUI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,11 +6,41 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    @State private var image: Image?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        //image = Image("quebec")
+        
+        guard let inputImage = UIImage(named: "quebec") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        let currentFilter = CIFilter.sepiaTone()
+        currentFilter.inputImage = beginImage
+        currentFilter.intensity = 1
+        //currentFilter.scale = 50 pixellate()
+        //currentFilter.radius = 200 crystallize()
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        let context = CIContext()
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiimg = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiimg)
+        }
+        
     }
 }
 
