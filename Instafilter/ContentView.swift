48fb5674570ccdeda85171f3de0c773d3096bbd2
Rafael Calunga <rafaelcalunga@gmail.com>
2020-11-11 22:01:16 -0500

    Cleanup

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,69 +6,12 @@
 //
 
 import SwiftUI
-import CoreImage
-import CoreImage.CIFilterBuiltins
-
-class ImageSaver: NSObject {
-    func writeToPhotoAlbum(image: UIImage) {
-        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
-    }
-
-    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        print("Save finished!")
-    }
-}
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var showingImagePicker = false
-    @State private var inputImage: UIImage?
     
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select Image") {
-                self.showingImagePicker = true
-            }
-        }
-        .onAppear(perform: loadImage)
-        .sheet(isPresented: $showingImagePicker, onDismiss: loadNewImage) {
-            ImagePicker(image: $inputImage)
-        }
-    }
-    
-    func loadNewImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
-        
-        //UIImageWriteToSavedPhotosAlbum(inputImage, nil, nil, nil)
-        let imageSaver = ImageSaver()
-        imageSaver.writeToPhotoAlbum(image: inputImage)
-    }
-    
-    func loadImage() {
-        //image = Image("quebec")
-        
-        guard let inputImage = UIImage(named: "quebec") else { return }
-        let beginImage = CIImage(image: inputImage)
-        
-        let currentFilter = CIFilter.sepiaTone()
-        currentFilter.inputImage = beginImage
-        currentFilter.intensity = 1
-        //currentFilter.scale = 50 pixellate()
-        //currentFilter.radius = 200 //crystallize()
-        guard let outputImage = currentFilter.outputImage else { return }
-        
-        let context = CIContext()
-        
-        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-            let uiimg = UIImage(cgImage: cgimg)
-            image = Image(uiImage: uiimg)
-        }
-        
+        Text("Hello World")
+            .padding()
     }
 }
 
